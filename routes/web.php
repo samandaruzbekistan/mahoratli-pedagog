8a1695e95b41c4af102d502b000d407b23a6ddb2
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2025-05-08 12:39:18 +0500

    Admin auth added

--- routes/web.php
+++ routes/web.php
@@ -28,27 +28,30 @@ Route::get('/theme/{theme_id}', [UserController::class, 'show_theme'])->name('us
 Route::prefix('admin')->group(function () {
     Route::view('/', 'admin.login')->name("admin.login");
     Route::post('login', [AdminController::class, 'auth'])->name('admin.auth');
-    Route::get('logout', [AdminController::class, 'logout'])->name('admin.logout');
-    Route::view('profile','admin.profile')->name('admin.profile');
-    Route::post('profile',[AdminController::class, 'update_password'])->name('admin.password.update');
 
-    Route::get('section/{id}', [AdminController::class, 'section'])->name('admin.view.section');
+    Route::middleware(['adminAuth'])->group(function () {
+        Route::get('logout', [AdminController::class, 'logout'])->name('admin.logout');
+        Route::view('profile', 'admin.profile')->name('admin.profile');
+        Route::post('profile', [AdminController::class, 'update_password'])->name('admin.password.update');
 
-    Route::get('theme-topic/{theme_id}', [TopicPdfController::class, 'show_theme_topics'])->name('theme.topic.view');
-    Route::post('add-topic', [TopicPdfController::class, 'new_topic'])->name('admin.topic.add');
-    Route::get('delete-topic/{id}', [TopicPdfController::class, 'delete_topic'])->name('admin.topic.delete');
+        Route::get('section/{id}', [AdminController::class, 'section'])->name('admin.view.section');
 
-    Route::get('theme-presentation/{theme_id}', [PresentationController::class, 'show_theme_topics'])->name('theme.presentation.view');
-    Route::post('add-presentation', [PresentationController::class, 'new_topic'])->name('admin.presentation.add');
-    Route::get('delete-presentation/{id}', [PresentationController::class, 'delete_topic'])->name('admin.presentation.delete');
+        Route::get('theme-topic/{theme_id}', [TopicPdfController::class, 'show_theme_topics'])->name('theme.topic.view');
+        Route::post('add-topic', [TopicPdfController::class, 'new_topic'])->name('admin.topic.add');
+        Route::get('delete-topic/{id}', [TopicPdfController::class, 'delete_topic'])->name('admin.topic.delete');
 
-    Route::get('show-section-dict-sections/{section_id}', [DictSectionController::class, 'show'])->name('admin.section.dict.sections');
-    Route::post('add-dict-section', [DictSectionController::class, 'new_section'])->name('admin.add.dict.section');
-    Route::get('show-section-dicts/{section_id}', [DictController::class, 'show_section_dicts'])->name('admin.theme.dicts');
-    Route::get('delete-section-dict/{dict_id}', [DictController::class, 'delete_dict'])->name('admin.delete.dict');
-    Route::post('add-dict', [DictController::class, 'new_dict'])->name('admin.add.dict');
+        Route::get('theme-presentation/{theme_id}', [PresentationController::class, 'show_theme_topics'])->name('theme.presentation.view');
+        Route::post('add-presentation', [PresentationController::class, 'new_topic'])->name('admin.presentation.add');
+        Route::get('delete-presentation/{id}', [PresentationController::class, 'delete_topic'])->name('admin.presentation.delete');
 
-    Route::get('show-theme-quizzes/{theme_id}', [QuizController::class, 'show_quizzes'])->name('admin.theme.quizzes');
-    Route::get('delete-theme-quiz/{quiz_id}', [QuizController::class, 'delete_quiz'])->name('admin.delete.quiz');
-    Route::post('add-quiz', [QuizController::class, 'add_quiz'])->name('admin.add.quiz');
+        Route::get('show-section-dict-sections/{section_id}', [DictSectionController::class, 'show'])->name('admin.section.dict.sections');
+        Route::post('add-dict-section', [DictSectionController::class, 'new_section'])->name('admin.add.dict.section');
+        Route::get('show-section-dicts/{section_id}', [DictController::class, 'show_section_dicts'])->name('admin.theme.dicts');
+        Route::get('delete-section-dict/{dict_id}', [DictController::class, 'delete_dict'])->name('admin.delete.dict');
+        Route::post('add-dict', [DictController::class, 'new_dict'])->name('admin.add.dict');
+
+        Route::get('show-theme-quizzes/{theme_id}', [QuizController::class, 'show_quizzes'])->name('admin.theme.quizzes');
+        Route::get('delete-theme-quiz/{quiz_id}', [QuizController::class, 'delete_quiz'])->name('admin.delete.quiz');
+        Route::post('add-quiz', [QuizController::class, 'add_quiz'])->name('admin.add.quiz');
+    });
 });
